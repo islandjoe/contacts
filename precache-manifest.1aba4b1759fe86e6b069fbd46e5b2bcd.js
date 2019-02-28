8731e0ab938adc4ff95456af92bd794fa42ecc93
Arthur Kho <islandjoe@gmail.com>
2019-02-28 20:37:56 +0200

    Updates

--- precache-manifest.1aba4b1759fe86e6b069fbd46e5b2bcd.js
+++ precache-manifest.1aba4b1759fe86e6b069fbd46e5b2bcd.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "2b7f312436ab9ae047be",
+    "revision": "e6a991c334e39eb9c043",
     "url": "/contacts/static/css/main.fd8d488d.chunk.css"
   },
   {
-    "revision": "2b7f312436ab9ae047be",
-    "url": "/contacts/static/js/main.2b7f3124.chunk.js"
+    "revision": "e6a991c334e39eb9c043",
+    "url": "/contacts/static/js/main.e6a991c3.chunk.js"
   },
   {
     "revision": "910f0a36f9528da6cf42",
     "url": "/contacts/static/js/runtime~main.910f0a36.js"
   },
   {
-    "revision": "819c1f4e38baf4ad7d9d",
+    "revision": "fef05c5144556ac872c8",
     "url": "/contacts/static/css/2.af6b662d.chunk.css"
   },
   {
-    "revision": "819c1f4e38baf4ad7d9d",
-    "url": "/contacts/static/js/2.819c1f4e.chunk.js"
+    "revision": "fef05c5144556ac872c8",
+    "url": "/contacts/static/js/2.fef05c51.chunk.js"
   },
   {
-    "revision": "9378acca72f6f505bca6f362526da191",
+    "revision": "3966ffe7d5bfebf5efee0fdc8878ee14",
     "url": "/contacts/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/contacts/precache-manifest.0aacc5e707115d0d0f069850865e5ae5.js"
+  "/contacts/precache-manifest.1aba4b1759fe86e6b069fbd46e5b2bcd.js"
 );
 
 workbox.clientsClaim();
